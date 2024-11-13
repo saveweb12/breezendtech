f1aacc58698d464236f61b8dafad5b084c01dffe
maheshsaveasweb <mahesh.saveasweb101@gmail.com>
2024-11-13 17:56:47 +0530

    added slider html layout

--- src/components/index/Slider.jsx
+++ src/components/index/Slider.jsx
@@ -6,10 +6,145 @@ function Slider({ data }) {
     <>
       {data.map((slider) => (
         <div key={slider.id}>
-          <h1>{slider.slider_name}</h1>
-          <img src={slider.slider_image} alt={slider.title} />
-          <p>{slider.description}</p>
-          <a href={slider.button_link}>{slider.button_text.trim()}</a>
+          <section className="slider style_one pag_position_two">
+            <div
+              className="banner_carousel owl-carousel owl_nav_none owl_dots_block theme_carousel owl-theme"
+              data-options='{"loop": true, "margin": 0, "autoheight":true, "lazyload":true, "nav": true, "dots": true, "autoplay": true, "autoplayTimeout": 7000, "smartSpeed": 1800, "responsive":{ "0" :{ "items": "1" }, "768" :{ "items" : "1" } , "1000":{ "items" : "1" }}}'
+            >
+              <div className="slide-item">
+                <div className="slide-item-content content_left">
+                  <div
+                    className="image-layer"
+                    style={{
+                      backgroundImage:
+                        "url(assets/images/sliders/slider-2-bg.jpg)",
+                    }}
+                  />
+                  <div className="auto-container">
+                    <div className="d-flex align-items-center">
+                      <div className="col-lg-7 col-md-12  col-sm-12 col-xs-12">
+                        <div className="slider_content">
+                          <h1>Software Consulting and Development</h1>
+                          <p className="description">
+                            Lorem ipsum dolor sit amet, consectetur
+                            adipisicingiu elit, sed do eiusmod tempor incididunt
+                            ut labore et dolore.
+                          </p>
+                          <a
+                            href="#"
+                            target="_blank"
+                            rel="nofollow"
+                            className="theme-btn one  animated _zoomIn"
+                          >
+                            Read More{" "}
+                          </a>
+                        </div>
+                      </div>
+                      <div className="col-lg-5 col-md-12  col-sm-12 col-xs-12 image_column">
+                        <div className="slider_image">
+                          <img
+                            src="assets/images/sliders/slider-2-1.png"
+                            className="img-fluid"
+                            alt="slider image"
+                          />
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="slide-item">
+                <div className="slide-item-content content_right">
+                  <div
+                    className="image-layer"
+                    style={{
+                      backgroundImage:
+                        "url(assets/images/sliders/slider-2-bg.jpg)",
+                    }}
+                  />
+                  <div className="auto-container">
+                    <div className="row align-items-center">
+                      <div className="col-lg-7 col-md-12  col-sm-12 col-xs-12">
+                        <div className="slider_content">
+                          <h1>We Help Businesses</h1>
+                          <h6>
+                            Organizations Reduce risk by providing employment
+                          </h6>
+                          <p className="description">
+                            Duty obligations of business it will frequently
+                            occur that pleasures have to be repudiated and
+                            annoyances accepted.
+                          </p>
+                          <a
+                            href="#"
+                            target="_blank"
+                            rel="nofollow"
+                            className="theme-btn one"
+                          >
+                            Read More{" "}
+                          </a>
+                        </div>
+                      </div>
+                      <div className="col-lg-5 col-md-12  col-sm-12 col-xs-12 image_column">
+                        <div className="slider_image">
+                          <img
+                            src="assets/images/sliders/slider-2-2.png"
+                            className="img-fluid"
+                            alt="slider image"
+                          />
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="slide-item">
+                <div className="slide-item-content content_left">
+                  <div
+                    className="image-layer"
+                    style={{
+                      backgroundImage:
+                        "url(assets/images/sliders/slider-2-bg.jpg)",
+                    }}
+                  />
+                  <div className="auto-container">
+                    <div className="row align-items-center">
+                      <div className="col-lg-7 col-md-12  col-sm-12 col-xs-12">
+                        <div className="slider_content">
+                          <h1>Recruit the best employees</h1>
+                          <h6>
+                            Our search results mitigate your company's exposure
+                          </h6>
+                          <p className="description">
+                            Duty obligations of business it will frequently
+                            occur that pleasures have to be repudiated and
+                            annoyances accepted.
+                          </p>
+                          <a
+                            href="#"
+                            target="_blank"
+                            rel="nofollow"
+                            className="theme-btn one"
+                          >
+                            Read More{" "}
+                          </a>
+                        </div>
+                      </div>
+                      <div className="col-lg-5 col-md-12  col-sm-12 col-xs-12 image_column">
+                        <div className="slider_image">
+                          <img
+                            src="assets/images/sliders/slider-2-3.png"
+                            className="img-fluid"
+                            alt="slider image"
+                          />
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
         </div>
       ))}
     </>
