8e980c50ddbbb57e67418dfa073b066852a65e05
dinesh <dinesh.saveasweb@gmail.com>
2025-03-24 11:07:03 +0530

    Save untracked files before merging

---
+++ src/app/about/OptimzeMarketingProcesses.jsx
@@ -0,0 +1,102 @@
+import React from 'react'
+
+const OptimzeMarketingProcesses = () => {
+  return (
+    <>
+      <section className="image-section bg_light_1">
+     {/*===============spacing==============*/}
+     <div className="pd_top_70" />
+     {/*===============spacing==============*/}
+     <div className="container">
+       <div className="row">
+         <div className="col-lg-1" />
+         <div className="col-lg-10">
+           <div className="description_box text-center">
+             <p>
+               The great explorer of the truth, the master-builder of human
+               happiness no one rejects dislikes avoids pleasure itself because
+               it is pleasure but because know who do not those how to pursue
+               pleasures rationally encounter consequences that are extremely
+               painful desires to obtain.{" "}
+             </p>
+           </div>
+           {/*===============spacing==============*/}
+           <div className="pd_bottom_50" />
+           {/*===============spacing==============*/}
+           <div className="row">
+             <div className="col-xl-4 col-lg-6 col-md-6 col-sm-12">
+               <div className="process_box style_four dark_color">
+                 <div className="process_box_outer_four ">
+                   <div className="icon">
+                     <img
+                       src="/images/icons/optimize.png"
+                       className="img-fluid svg_image"
+                       alt="icon png"
+                     />
+                   </div>
+                   <div className="content_box">
+                     <h2>
+                       <a href="#" rel="nofollow">
+                         OPTIMIZE
+                       </a>
+                     </h2>
+                     <p>Marketing Processes</p>
+                   </div>
+                 </div>
+               </div>
+             </div>
+             <div className="col-xl-4 col-lg-6 col-md-6 col-sm-12">
+               <div className="process_box style_four dark_color">
+                 <div className="process_box_outer_four ">
+                   <div className="icon">
+                     <img
+                       src="/images/icons/generate.png"
+                       className="img-fluid svg_image"
+                       alt="icon png"
+                     />
+                   </div>
+                   <div className="content_box">
+                     <h2>
+                       <a href="#" rel="nofollow">
+                         GENERATE
+                       </a>
+                     </h2>
+                     <p>Targeted Results</p>
+                   </div>
+                 </div>
+               </div>
+             </div>
+             <div className="col-xl-4 col-lg-6 col-md-6 col-sm-12">
+               <div className="process_box style_four dark_color">
+                 <div className="process_box_outer_four ">
+                   <div className="icon">
+                     <img
+                       src="/images/icons/always-getting-better.png"
+                       className="img-fluid svg_image"
+                       alt="icon png"
+                     />
+                   </div>
+                   <div className="content_box">
+                     <h2>
+                       <a href="#" rel="nofollow">
+                         GROW
+                       </a>
+                     </h2>
+                     <p>Your Brand Online</p>
+                   </div>
+                 </div>
+               </div>
+             </div>
+           </div>
+           {/*===============spacing==============*/}
+           <div className="pd_bottom_50" />
+           {/*===============spacing==============*/}
+         </div>
+       </div>
+     </div>
+   </section>
+    </>
+  ) 
+}
+
+export default OptimzeMarketingProcesses
